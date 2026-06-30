67720ad0985627886d66e92e84f777319af4f62f
sashaistin2015-wq <sashaistin2015@gmail.com>
2026-06-30 17:10:17 +0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -3,18 +3,20 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const SUPABASE_URL = 'https://plejzaehtlcesxszugcx.supabase.co';
 const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsZWp6YWVodGxjZXN4c3p1Z2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MDkxNzcsImV4cCI6MjA5ODI4NTE3N30.xWlI0egd7wLjyCcv30_Gr95hVv4jShdrIAxo_IaKrNM';
 
-const apiProxy = createProxyMiddleware({
+const proxy = createProxyMiddleware({
   target: SUPABASE_URL,
   changeOrigin: true,
-  pathRewrite: { '^/supabase': '' },  // убираем /supabase из пути
+  pathRewrite: (path) => path.replace(/^\/supabase/, ''),
   on: {
     proxyReq: (proxyReq, req) => {
       proxyReq.setHeader('apikey', ANON_KEY);
       if (req.headers.authorization) {
         proxyReq.setHeader('Authorization', req.headers.authorization);
+      } else {
+        proxyReq.setHeader('Authorization', `Bearer ${ANON_KEY}`);
       }
     }
   }
 });
 
-module.exports = (req, res) => apiProxy(req, res);
+module.exports = (req, res) => proxy(req, res);
