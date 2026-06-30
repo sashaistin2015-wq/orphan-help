75b56b085dc4ba8d8ed7f0ef13ec7f7589d33979
sashaistin2015-wq <sashaistin2015@gmail.com>
2026-06-30 17:01:21 +0300

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const SUPABASE_URL = 'https://plejzaehtlcesxszugcx.supabase.co';
+const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsZWp6YWVodGxjZXN4c3p1Z2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MDkxNzcsImV4cCI6MjA5ODI4NTE3N30.xWlI0egd7wLjyCcv30_Gr95hVv4jShdrIAxo_IaKrNM';
+
+const apiProxy = createProxyMiddleware({
+  target: SUPABASE_URL,
+  changeOrigin: true,
+  pathRewrite: { '^/supabase': '' },  // убираем /supabase из пути
+  on: {
+    proxyReq: (proxyReq, req) => {
+      proxyReq.setHeader('apikey', ANON_KEY);
+      if (req.headers.authorization) {
+        proxyReq.setHeader('Authorization', req.headers.authorization);
+      }
+    }
+  }
+});
+
+module.exports = (req, res) => apiProxy(req, res);
